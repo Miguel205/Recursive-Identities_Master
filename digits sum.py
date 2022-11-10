f55d4292fe91f08af8486958bb24952007e17421
BenShefter <94476996+BenShefter@users.noreply.github.com>
2022-11-10 09:09:07 -0500

    Update digits sum.py

--- digits sum.py
+++ digits sum.py
@@ -11,10 +11,8 @@ Description: Finds the factorial of a number given
 
 '''
 
-n = 5
-
 def sum_of_digits(n):
     if n < 10:
         return n    
     else:
-         return sum_of_digits(n + 10) + n%10
+         return sum_of_digits(int(n/10)) + n%10
