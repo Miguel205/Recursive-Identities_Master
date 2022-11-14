2270cd8e47811f83e7005d6e4eb96e1abcb1eda0
Miguel205 <94477305+Miguel205@users.noreply.github.com>
2022-11-13 23:22:48 -0500

    Update digits sum.py

--- digits sum.py
+++ digits sum.py
@@ -3,15 +3,15 @@
 def sum_of_digits(n):
     
     
-'''
+    '''
 
-@author: BShefter25
+    @author: BShefter25
 
-Created on Oct 1, 2022
+    Created on Oct 1, 2022
 
-Bugs: There are no bugs
+    Bugs: There are no bugs
 
-'''
+    '''
     
     if n < 10:                                              #if n is less than 10
         return n                                            #return the number
