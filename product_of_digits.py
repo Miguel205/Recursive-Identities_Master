5790a75167a57d10aa5fca3dd2d03da5f7c40c95
Miguel205 <94477305+Miguel205@users.noreply.github.com>
2022-11-13 23:44:11 -0500

    Add files via upload

---
+++ product_of_digits.py
@@ -0,0 +1,11 @@
+def product_of_numbers_digits(n):
+    '''
+    @author: mdominguez25
+    Created on Nov 11, 2022
+    Bugs:
+    '''
+
+    if n < 10:  # if n < than 10
+        return n  # return the n
+    else:  # otherwise
+        return product_of_numbers_digits(int( n /10)) * n% 10  # return the puduct of the remander of n/10 plus the out put of f(n/10)
